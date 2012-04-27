c2af3e1d5a1e8ff1b6c156c05fa4bdec7e8fd619
bjbbarr <brian.barr@meltmedia.com>
2012-04-27 16:55:19 -0700

    fleshed out FileServlet

--- jgroups/src/main/java/com/meltmedia/cadmium/jgroups/ContentService.java
+++ jgroups/src/main/java/com/meltmedia/cadmium/jgroups/ContentService.java
@@ -3,6 +3,6 @@ package com.meltmedia.cadmium.jgroups;
 import java.io.File;
 
 public interface ContentService {
-  public void switchContent(File newDir);
+  public void switchContent(String newDir);
   public void setListener(ContentServiceListener listener);
 }
--- servlets/src/main/java/com/meltmedia/cadmium/servlets/FileServlet.java
+++ servlets/src/main/java/com/meltmedia/cadmium/servlets/FileServlet.java
@@ -1,8 +1,55 @@
 package com.meltmedia.cadmium.servlets;
 
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import com.meltmedia.cadmium.jgroups.ContentService;
+import com.meltmedia.cadmium.jgroups.ContentServiceListener;
+
 @SuppressWarnings("serial")
-public class FileServlet 
-  extends net.balusc.webapp.FileServlet
-{
+public class FileServlet extends net.balusc.webapp.FileServlet implements ContentService {
+	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	protected ContentServiceListener listener;
+	
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+
+		config.getServletContext().setAttribute(ContentService.class.getName(), this);
+		
+		if (config.getInitParameter("pathPrefix") != null) {
+			this.setBasePath(config.getInitParameter("pathPrefix"));
+		}
+	}	
+	
+	
+	@Override
+	public void switchContent(String newDir) {
+		
+		try {
+			
+			this.setBasePath(newDir);
+			
+		} catch (ServletException e) {
+			
+			log.error("Problem while setting new directory: {}", e);
+			
+		}
+		
+		listener.doneSwitching();
+		
+	}
+
+	@Override
+	public void setListener(ContentServiceListener listener) {
+		
+		this.listener = listener;		
+	}
 
 }
