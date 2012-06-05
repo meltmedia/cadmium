20304c1f7a6196411a45a0c9d7f8bedede1fa5d9
bjbbarr <brian.barr@meltmedia.com>
2012-06-05 12:24:24 -0700

    fixed import issue

--- cli/src/main/java/com/meltmedia/cadmium/cli/StatusCommand.java
+++ cli/src/main/java/com/meltmedia/cadmium/cli/StatusCommand.java
@@ -1,10 +1,7 @@
 package com.meltmedia.cadmium.cli;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
