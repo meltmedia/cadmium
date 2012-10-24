954220f1ab5612eeefdf6bc8ee48bc705953279b
John McEntire <john.mcentire@meltmedia.com>
2012-10-24 14:44:59 -0700

    Checking in new Utils class.

---
+++ core/src/main/java/com/meltmedia/cadmium/core/util/Jsr250Utils.java
@@ -0,0 +1,13 @@
+package com.meltmedia.cadmium.core.util;
+
+import org.slf4j.Logger;
+
+public final class Jsr250Utils {
+  public static void postConsrtuct(Object obj, Logger log) throws Exception {
+    
+  }
+  
+  public static void preDestroy(Object obj, Logger log) throws Exception {
+    
+  }
+}
