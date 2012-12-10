6ef256e9e780da653a7f51dfaed292fd12b4be9d
John McEntire <john.mcentire@meltmedia.com>
2012-12-10 14:13:33 -0700

    Updated the jgroups aws version

--- deployer/src/main/java/com/meltmedia/cadmium/deployer/DeployerService.java
+++ deployer/src/main/java/com/meltmedia/cadmium/deployer/DeployerService.java
@@ -25,7 +25,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.eclipse.jgit.util.StringUtils;
 
