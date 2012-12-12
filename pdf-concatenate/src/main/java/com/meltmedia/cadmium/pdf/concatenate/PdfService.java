df2024cdaf786bec9784c597a682803605841468
Chris Haley <chris.haley@meltmedia.com>
2012-12-12 16:00:31 -0700

    done and tested BRAF-458

--- pdf-concatenate/src/main/java/com/meltmedia/cadmium/pdf/concatenate/PdfService.java
+++ pdf-concatenate/src/main/java/com/meltmedia/cadmium/pdf/concatenate/PdfService.java
@@ -6,12 +6,14 @@ import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,15 +33,18 @@ public class PdfService {
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	private Pattern ownerPasswordPattern;
 	
+	@GET
 	@Path("{fileName}")
-	public Response pdfService(@PathParam("fileName") String fileName, @QueryParam("srcUrl") String[] srcUrls,@Context HttpServletResponse resp) {
+	public Response pdfService(@PathParam("fileName") String fileName, @Context HttpServletRequest req) {
+		String[] srcUrls = req.getParameterValues("srcUrl");
+	  log.info("Building pdf {} from {}", fileName, srcUrls);
+		StreamingOutput response = null;
+		
 		if (srcUrls != null && srcUrls.length > 0) {
 
-      String currentUrl = "";
       List<PdfFile> fileList = new LinkedList<PdfFile>();
       try {
         for (String url : srcUrls) {
-          currentUrl = url;
 
           String password = null;
           Matcher m = getOwnerPasswordPattern().matcher(url);
@@ -57,13 +62,14 @@ public class PdfService {
             fileList.add(new PdfFile(new URL(url), null));
           }
         } 
-
-       concatenate(fileList, resp.getOutputStream());
+        log.debug("Creating return object for src {}", fileList);
+        response = new PdfStreamingOutput(fileList);
       }catch(Exception e) {
-      	
+      	return Response.status(Response.Status.BAD_REQUEST).build();
       }
+      
 		}
-		return Response.ok().build();
+		return Response.ok(response).header("Content-Disposition", "attachment; filename=" + fileName).build();
 	}
 
 	private Pattern getOwnerPasswordPattern() {
@@ -72,51 +78,71 @@ public class PdfService {
     }
     return ownerPasswordPattern;
   }
+	
+	private class PdfStreamingOutput implements StreamingOutput {
 		
-	public void concatenate(List<PdfFile> pdfFiles, OutputStream os) throws IOException, DocumentException {
+		private List<PdfFile> pdfFiles;
+		
+		public PdfStreamingOutput(List<PdfFile> pdfFiles) {
+			this.pdfFiles = pdfFiles;
+		}
+		
+		public void concatenate(OutputStream os) throws IOException, DocumentException {
+	    log.debug("Concatenating {}", pdfFiles);
+	    Document document = new Document();
+	
+	    PdfCopy copy = new PdfCopy(document, os);
+	
+	    document.open();
+	
+	    PdfReader reader = null;
+	    int pageCount;
+	    String path = null;
+	
+	    try {
+	      for (PdfFile f : pdfFiles) {
+	        path = f.getUrl().toString();
+	
+	        if (f.getPassword() != null) {
+	          reader = new PdfReader(f.getUrl(), f.getPassword().getBytes());
+	        } else {
+	          reader = new PdfReader(f.getUrl());
+	        }
+	
+	        pageCount = reader.getNumberOfPages();
+	        for (int page = 1; page <= pageCount; page++) {
+	          copy.addPage(copy.getImportedPage(reader, page));
+	        }
+	        reader.close();
+	        reader = null;
+	      }
+	      document.close();
+	      document = null;
+	    } catch (IOException ioe) {
+	      log.error("Error reading pdf: " + path, ioe);
+	      throw ioe;
+	    } catch (DocumentException de) {
+	    	log.error("Error processing pdf: " + path, de);
+	      throw de;
+	    } catch (RuntimeException re) {
+	    	log.error("Runtime Error processing pdf: " + path, re);
+	      throw re;
+	    } finally {
+	      if (reader != null) reader.close();
+	      if (document != null) document.close();
+	    }
+	  }
 
-    Document document = new Document();
-
-    PdfCopy copy = new PdfCopy(document, os);
-
-    document.open();
-
-    PdfReader reader = null;
-    int pageCount;
-    String path = null;
-
-    try {
-      for (PdfFile f : pdfFiles) {
-        path = f.getUrl().toString();
-
-        if (f.getPassword() != null) {
-          reader = new PdfReader(f.getUrl(), f.getPassword().getBytes());
-        } else {
-          reader = new PdfReader(f.getUrl());
-        }
-
-        pageCount = reader.getNumberOfPages();
-        for (int page = 1; page <= pageCount; page++) {
-          copy.addPage(copy.getImportedPage(reader, page));
-        }
-        reader.close();
-        reader = null;
-      }
-      document.close();
-      document = null;
-    } catch (IOException ioe) {
-      log.error("Error reading pdf: " + path, ioe);
-      throw ioe;
-    } catch (DocumentException de) {
-    	log.error("Error processing pdf: " + path, de);
-      throw de;
-    } catch (RuntimeException re) {
-    	log.error("Runtime Error processing pdf: " + path, re);
-      throw re;
-    } finally {
-      if (reader != null) reader.close();
-      if (document != null) document.close();
-    }
-  }
+		@Override
+		public void write(OutputStream os) throws IOException,
+				WebApplicationException {
+			try {
+				concatenate(os);
+			} catch(DocumentException e) {
+				log.warn("Failed to build document.", e);
+				throw new WebApplicationException(e);
+			}
+		}
+	}
 
 }
