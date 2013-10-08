e0fe89798af8b802509a0f95170f3379ed1e3621
John McEntire <john.mcentire@meltmedia.com>
2013-10-08 10:00:00 -0700

    Updated script to setup a site on cadmium in vagrant

--- deployment/provisioning/etc/setup-vagrant.sh
+++ deployment/provisioning/etc/setup-vagrant.sh
@@ -10,3 +10,8 @@ else
   sudo chown -R ${SUDO_USER}:${SUDO_USER} bin
   sudo -u ${SUDO_USER} ./bin/setup.sh "$@"
 fi
+
+if !(grep vagrant /etc/hosts > /dev/null 2> /dev/null); then
+  sudo sed -i'' "/precise64/ a\
+127.0.0.1 vagrant" /etc/hosts
+fi
