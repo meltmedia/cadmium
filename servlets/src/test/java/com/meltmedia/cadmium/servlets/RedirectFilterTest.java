734ef3d37e92b4d6cd6f870a80bc9a94db61b5d0
brendanerquiaga <brendanerquiaga@gmail.com>
2016-09-28 12:43:56 -0700

    Redirect Filter Tests

--- servlets/src/test/java/com/meltmedia/cadmium/servlets/RedirectFilterTest.java
+++ servlets/src/test/java/com/meltmedia/cadmium/servlets/RedirectFilterTest.java
@@ -2,19 +2,44 @@ package com.meltmedia.cadmium.servlets;
 
 import org.junit.Test;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 public class RedirectFilterTest {
 
+    private String[] inputRequests = {
+        "http://domain.com/some/path/with?params=true",
+        "http://domain.com/some/path/without/",
+        "http://domain.com/some/path?extra=1"
+    };
+
+    private String[] expectedResponses = {
+        "http://domain.com/redirected/path?params=true",
+        "http://domain.com/redirected/path/with?params=true",
+        "http://domain.com/redirected/path/with?params=true&extra=1"
+    };
+
     @Test
     public void testDoFilter() throws Exception {
+        RedirectFilter filter = new RedirectFilter();
 
-        //If path = http://domain.com/some/path?params=true
-        //  redir should = http://domain.com/redirected/path?params=true
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ServletOutputStream out = mock(ServletOutputStream.class);
+        when(response.getOutputStream()).thenReturn(out);
+        FilterChain chain = mock(FilterChain.class);
 
-        //If path = http://domain.com/some/path/without/params
-        //  redir should = http://domain.com/redirected/path/with?params=true
+        ServletRequest request; //add each inputRequest
 
-        //If path = http://domain.com/some/path?extra=1
-        //  redir should = http://domain.com/redirected/path/with?params=true&extra=1
+        filter.doFilter(request, response,chain);
+
+        verify(response).setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        verify(response).setHeader("Location", expectedResponses);
     }
 }
